0c53d12786b75bd7ded608ef962e3cb8b212e8a4
KJayce <jaycekaje@gmail.com>
2017-04-29 19:03:11 +0200

    adding files

---
+++ Cadavre.php
@@ -0,0 +1,26 @@
+<?php $name=$_GET['name'];
+    $likes=$_GET['likes'];
+    echo'<pre>';
+    print_r($_GET);
+    echo '</pre>';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST">
+    
+    <input type="text" name="name">
+    <input type="text" name="likes">
+    <input type="submit">
+
+    </form>
+    <h1><?php echo $name;?> aime <?php echo $likes; ?></h1>
+     
+</body>
+</html>
