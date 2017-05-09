1cbad17c234b37699c9ecbf95e0031ad6b3baeba
KJayce <jaycekaje@gmail.com>
2017-05-09 23:39:00 +0200

    major php breakout

--- excuse.php
+++ excuse.php
@@ -10,33 +10,86 @@
     <title>Document</title>
 </head>
 <body>
+    <?php 
+        $SnameErr = $TnameErr = $GenderErr = $MotiveErr = "";
+        $Sname = $Tname = $Gender = $Motive = "";
+
+        if ($_SERVER["REQUEST_METHOD"]=="POST"){
+            if (empty($_POST["Sname"])){
+                $SnameErr = "Student's name is required";
+            } else {
+                $Sname = test_input($_POST["Sname"]);
+                if (!preg_match("/^[a-zA-Z ]*$/", $Sname)){
+                    $SnameErr = "Only letters and white space allowed";
+                }
+            }
+
+
+        if ($_SERVER["REQUEST_METHOD"]=="POST"){
+            if (empty($_POST["Tname"])){
+                $TnameErr = "Teacher's name is required";
+            } else {
+                $Tname = test_input($_POST["Tname"]);
+                if (!preg_match("/^[a-zA-Z ]*$/", $Tname)){
+                    $TnameErr = "Only letters and white space allowed";
+                }
+            }
+
+
+        if (empty($_POST["Gender"])) {
+            $GenderErr = "Gender is required";
+                } else {
+                    $Gender = test_input($_POST["Gender"]);
+                }
+            }
+
+        if ($_POST["Motive"] == "."){
+            $MotiveErr = "An unnatendance motive is required";
+            }   else {
+                $Motive = test_input ($_POST["Motive"]);
+            }
+        }
+
+        function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+}
+
+?>
 
 <h1 class= "page-header">Unattendance Motive</h1>
+<p><span>required field</span></p>
 
-<form class"form-horizontal" name="form1" method="POST" action="">
+<form class"form-horizontal" name="form1" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 <section class="container-fluid">
         <h2 class="panel-info"><div class="panel-body">Please complete following form</div></h2>
             <div class="form-group">
                 <label class="control-label col-sm-2" for="Sname">Student Name:
                 </label>
 
-                <div class="col-sm-10"> <input type="name" id="Sname" placeholder="Enter Student Name...">
+                <div class="col-sm-10"> <input type="text" id="Sname" placeholder="Enter Student Name..." value="<?php echo $Sname; ?>">
                 </div>  
             </div>
+
+            <span class="error" >* <?php echo $SnameErr; ?></span>
 
             <div class="form-group">
                 <label class="control-label col-sm-2" for="Tname">Teacher Name:
                 </label>
 
-                <div class="col-sm-10"> <input type="name" id="Tname" placeholder="Enter Teacher Name...">
+                <div class="col-sm-10"> <input type="text" id="Tname" placeholder="Enter Teacher Name..." value="<?php echo $Tname; ?>" >
                 </div>
             </div>
             
+            <span class="error" >*<?php echo $TnameErr; ?></span>
+
             <div class="form-group">
-                <label class="control-label col-sm-2" for="Ex">Motive:
+                <label class="control-label col-sm-2" for="Motive">Motive:
                 </label>
                 <div class="col-sm-10"> 
-                    <select id="Ex">
+                    <select id="Motive">
                         <<option value=".">-</option>
                         <option value="A"> Sick</option>
                         <option value="B"> Dead Pet</option>
@@ -45,23 +98,25 @@
                     </select> <br>
 
                 <div id="gndr" class="form-group">
-                    <label class="control-label col-sm-2" for="gender">Gender:</label>
+                    <label class="control-label col-sm-2" for="Gender">Gender:</label>
                         <div class="col-sm-10">
-                        <input class="form-inline" type="radio" name="Gender" value="M">Male
-                        <input class="form-inline" type="radio" name="Gender" value="F">Female
+                        <input class="form-inline" type="radio" name="Gender"<?php if (isset($gender) && $gender=="male") echo "checked";?> value="M">Male
+                        <input class="form-inline" type="radio" name="Gender"<?php if (isset($gender) && $gender=="female") echo "checked";?> value="F">Female
                         </div>
                 </div>  
 
                     <div class="form-group"> 
                         <div class="col-sm-offset-2 col-sm-10">
-                        <button type="submit" class="btn btn-default">Submit</button>
+                        <button type="submit" value="submit" class="btn btn-default">Submit</button>
                         </div>
                     </div>
                 </div>
     </section>
+    <h3 class="jumbotron">Dear <?php echo($_POST['Tname']); ?>.</h3>
+        <p>I would like to apologize for my  
     </form>
 
-<h3 class="jumbotron">Dear <?php echo($_GET['Tname']); ?>.</h3>
+
 
 
 
