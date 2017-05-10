b44cb3d7c5c423cf8c48c13a8a13a92f5cefd18e
KJayce <jaycekaje@gmail.com>
2017-05-10 15:13:46 +0200

    jayce addition to the php

--- excuse.php
+++ excuse.php
@@ -1,4 +1,6 @@
 <?php 
+/*sanitization fct */
+
         function test_input($data) {
         $data = trim($data);
         $data = stripslashes($data);
@@ -17,19 +19,23 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
         crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="motive.css">
     <title>Document</title>
 </head>
 <body>
     <?php 
 
-        $SnameErr = $TnameErr = $GenderErr = $MotiveErr = "";
+        $SnameErr = $TnameErr = $GenderErr = $MotiveErr = ""; /*initiate empty variable */
         $Gender = $Motive = "";
 
-        $Sname =  $_POST['Sname'];
+        $Sname =  $_POST['Sname'];  /* give value to variables who need one */
         $Tname =  $_POST['Tname'];
 
-        if ($_SERVER["REQUEST_METHOD"]=="POST"){
+        if ($_SERVER["REQUEST_METHOD"]=="POST"){ /* state in an if the request method of server*/
+
+        /* everything below are all the conditions necessary for the form to be submitted */
             if (empty($_POST["Sname"])){
                 $SnameErr = "Student's name is required";
             } else {
@@ -51,9 +57,8 @@
             }
 
 
-        if (!isset($_POST["Gender"])) {
+        if ($_POST["Gender"] == null) {
             $GenderErr = "Gender is required";
-            return;
                 } else {
                     $Gender = test_input($_POST["Gender"]);
                 }
@@ -71,6 +76,10 @@
 ?>
 
 <h1 class= "page-header">EXGen</h1>
+
+<fieldset>
+    
+
 <p><span class="error"> * Required field</span></p>
 
 <form class"form-horizontal" name="form1" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
@@ -81,20 +90,22 @@
                 </label>
 
                 <div class="col-sm-10"><input type="text" name="Sname" placeholder="Enter Student Name..." value="<?php echo $Sname; ?>">
+                <span class="error" >* <?php echo $SnameErr; ?></span>
                 </div>  
             </div>
 
-            <span class="error" >* <?php echo $SnameErr; ?></span>
+            
 
             <div class="form-group">
                 <label class="control-label col-sm-2" for="Tname">Teacher Name:
                 </label>
 
                 <div class="col-sm-10"> <input type="text" name="Tname" placeholder="Enter Teacher Name..." value="<?php echo $Tname; ?>" >
+                <span class="error" >*<?php echo $TnameErr; ?></span>
                 </div>
             </div>
             
-            <span class="error" >*<?php echo $TnameErr; ?></span>
+            
 
             <div class="form-group">
                 <label class="control-label col-sm-2" for="Motive">Motive:
@@ -106,46 +117,67 @@
                         <option value="Dead Pet"> Dead Pet</option>
                         <option value="Family issues"> Family issues</option>
                         <option value="Extra-curricular Activities"> Extra-curricular Activities</option>
-                    </select> <br>
-            <span class="error">*<?php echo $MotiveErr; ?></span>
+                        
+                    </select> <span class="error">*<?php echo $MotiveErr; ?></span><br>
+            
 
-                <div id="gndr" class="form-group">
+                <div class="form-group">
                     <label class="control-label col-sm-2" for="Gender">Gender:</label>
-                        <div class="col-sm-10">
-                        <input class="form-inline" type="radio" name="Gender"<?php if (isset($gender) && $gender=="male") echo "checked";?> value="Male">Male
-                        <input class="form-inline" type="radio" name="Gender"<?php if (isset($gender) && $gender=="female") echo "checked";?> value="Female">Female
-                        <span class="error">* <?php echo $genderErr;?></span>
+                        <div class="col-sm-10" class="pull-left">
+                        <input class="form-inline" type="radio" name="Gender"<?php if (isset($Gender) && $Gender=="male") echo "checked";?> value="Male">Male <br>
+                        <input class="form-inline" type="radio" name="Gender"<?php if (isset($Gender) && $Gender=="female") echo "checked";?> value="Female">Female
+                        <span class="error">* <?php echo $GenderErr;?></span>
+                        <br><br>
+                        </div>
+                </div>  
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="TGender"> Teacher's Gender:</label>
+                        <div class="col-sm-10" class="pull-left">
+                        <input class="form-inline" type="radio" name="TGender"<?php if (isset($TGender) && $TGender=="male") echo "checked";?> value="Male">Male <br>
+                        <input class="form-inline" type="radio" name="TGender"<?php if (isset($TGender) && $TGender=="female") echo "checked";?> value="Female">Female
+                        <span class="error">* <?php echo $GenderErr;?></span>
                         <br><br>
                         </div>
                 </div>  
 
                     <div class="form-group"> 
                         <div class="col-sm-offset-2 col-sm-10">
-                        <button type="submit" name="submit" value="submit" class="btn btn-success">Submit</button>
+                        <a href="https://www.youtube.com/" target="_blank"><button type="submit" name="submit" value="submit" class="btn btn-success">Submit</button></a>
                         </div>
                     </div>
                 </div>
     </section>
-
-        <?php 
-    echo $Tname;
-    
-    echo "<br>";
-    echo $Sname;
-    
-    echo "<br>";
-    echo $Gender;
-    echo "<br>";
-    echo $Motive;
-    ?>
-
     
     </form>
+    </fieldset>
 
 
 
 
 
 
+
+
+
+
+
+
+    <?php
+
+    if (!preg_match("/^a-zA-Z*$/", $Tname)){
+                $Tname='';
+                }else echo $Tname;
+    
+        echo "<br>";
+    if (!preg_match ("/^a-zA-Z*$/", $Sname)){
+                $Sname='';
+                }else echo $Sname;
+    
+        echo "<br>";
+        echo $Gender;
+        echo "<br>";
+        echo $Motive;
+    ?>
 </body>
 </html>
